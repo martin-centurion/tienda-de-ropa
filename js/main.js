6e70f9efed4a2ddf6a71e2fef306b1b2e6d55ddd
martin-centurion <martinalejandrocenturion@gmail.com>
2022-12-28 00:40:58 -0300

    agregando dom / eventos / funciones

--- js/main.js
+++ js/main.js
@@ -1,74 +1,178 @@
+/* TU ALMACEN.COM - CARRITO DE COMPRAS */
 
-class Cliente {
-    constructor(nombre, direccion, telefono, mail){
-        this.nombre = nombre;
-        this.direccion = direccion;
-        this.telefono = telefono;
-        this.mail = mail;
-    }
-}
+//1) Mostrar productos en el HTML de forma dinamica.
+//2) Agregar productos en el carrito.
+//3) Evitar la carga de productos repetidos en el carrito.
+//4) Mostrar el carrito en el HTML de forma dinamica.
+//5) Eliminar productos del carrito.
+//6) Calcular el total de la compra.
+//7) Vaciar carrito.
+//8) Guardar el carrito en el localStorage.
 
-const clienteUno = new Cliente("Marcos", "Francia 4260, Florida Oeste, Provincia de Buenos Aires", 5491156355415, "marcos@gmail.com");
-const clienteDos = new Cliente("Vanesa", "Leloir 549, Marcos Paz, Provincia de Buenos Aires", 5492525321, "vanesa@gmail.com");
-
-const arrayClientes = [clienteUno, clienteDos];
-
-/* PRODUCTOS */
+/////////////////////////////////////
 
 class Producto {
-    constructor(nombre, precio, id){
+    constructor(id, nombre, precio, img){
+        this.id = id;
         this.nombre = nombre;
         this.precio = precio;
-        this.id = id;
+        this.img = img;
+        this.cantidad = 1;
     }
 }
 
-const buzoUno = new Producto("Buzo1", 12400, 1);
-const buzoDos = new Producto("Buzo2", 13000, 2);
-const buzoTres = new Producto("Buzo3", 12000, 3);
-const buzoCuatro = new Producto("Buzo4", 13100, 4);
+const productoUno = new Producto(1, "Buzo Azul", 14000, "../img/productos/buzos/buzo-azul.jpg");
+const productoDos = new Producto(2, "Buzo Marrón", 14500, "../img/productos/buzos/buzo-marron.jpg");
+const productoTres = new Producto(3, "Buzo Negro", 15000, "../img/productos/buzos/buzo-negro.jpg");
+const productoCuatro = new Producto(4, "Puffer Blanco", 14000, "../img/productos/puffer/puffer-blanco.jpg");
+const productoCinco = new Producto(5, "Puffer Rosa", 14500, "../img/productos/puffer/puffer-rosa.jpg");
+const productoSeis = new Producto(6, "Puffer Violeta", 15000, "../img/productos/puffer/puffer-violeta.jpg");
 
-const articulos = [buzoUno, buzoDos, buzoTres, buzoCuatro];
+const productos = [productoUno, productoDos, productoTres, productoCuatro, productoCinco, productoSeis];
 
-const arrayProductos = [];
+//Creamos el Array del Carrito
 
-function menu (){
-    alert("Bienvenidos a Tienda de Ropa.");
-    const opcion = parseInt(prompt("Ingrese una opción: \n1) Nuestra tienda.\n2)Finalizar Compra\n3)Salir."));
-    return opcion;
+let carrito = [];
+
+/* localStorage */
+
+if(localStorage.getItem("carrito")){
+    carrito = JSON.parse(localStorage.getItem("carrito"));
 }
 
+//Modificamos el DOM mostrando los productos:
 
-function carrito () {
-    let usuarioCompra = parseInt(prompt("Escriba el id del producto que desea adquirir: "));
-    let productoElegido = articulos.find (el => el.id === usuarioCompra);
-    console.log(productoElegido);
-    if(productoElegido === undefined){
-        alert("Ingrese un numero valido por favor");
-        carrito();
+const contenedorProductos = document.getElementById("contenedorProductos");
+
+//Creamos una funcion para mostrar los productos:
+
+const mostrarProductos = () => {
+    productos.forEach( producto => {
+        const card = document.createElement("div");
+        card.classList.add("productos");
+        card.innerHTML = `
+                <div>
+                <div>
+                    <img src="${producto.img}" class="productos_img" alt="${producto.nombre}">
+                    <div class= "">
+                        <h2>${producto.nombre}</h2>
+                        <h3> $${producto.precio} </h3>
+                        <a class"btn" id="boton ${producto.id}"> Agregar al Carrito </a>
+                    </div>
+                </div>
+                </div>
+                        `
+        contenedorProductos.appendChild(card);
+
+        //Agregar productos al carrito:
+        const boton = document.getElementById(`boton ${producto.id}`);
+        boton.addEventListener("click", () => {
+            agregarAlCarrito(producto.id);
+        })
+
+    })
+}
+
+mostrarProductos();
+
+//Creamos la funcion agregar al carrito:
+
+const agregarAlCarrito = (id) => {
+    const productoEnCarrito = carrito.find(producto => producto.id === id);
+    if(productoEnCarrito){
+        productoEnCarrito.cantidad++;
+    } else {
+        const producto = productos.find(producto => producto.id === id);
+        carrito.push(producto);
     }
-    arrayProductos.push(productoElegido.precio)
+    //Trabajamos con el localStorage
+    localStorage.setItem("carrito", JSON.stringify(carrito));
+    calcularTotal();
 }
 
-function finalizarCompra () {
-    let totalPrecio = arrayProductos.reduce((acumulador, producto) => acumulador + producto, 0);
-    alert("El total de su compra es " + totalPrecio + " ETH")
-    alert("Gracias por elegir Mi-Tienda para adquirir NFT's =)")
+//Mostrar el carrito de compras
+
+const contenedorCarrito = document.getElementById("contenedorCarrito");
+const verCarrito = document.getElementById("verCarrito");
+
+verCarrito.addEventListener("click", () => {
+    mostrarCarrito();
+})
+
+//Funcion para mostrar el carrito:
+
+const mostrarCarrito = () => {
+    contenedorCarrito.innerHTML = "";
+
+    carrito.forEach(producto => {
+        const card = document.createElement("div");
+        card.classList.add("productos");
+        card.innerHTML = `
+                <div>
+                <div>
+                    <img src="${producto.img}" class="productos_img" alt="${producto.nombre}">
+                    <div class= "">
+                        <h2>${producto.nombre}</h2>
+                        <h3> $${producto.precio} </h3>
+                        <h3> Cantidad ${producto.cantidad} </h3>
+                        <a id="eliminar ${producto.id}">Eliminar producto</a>
+                    </div>
+                </div>
+                </div>
+                        `
+
+        contenedorCarrito.appendChild(card);
+
+        //Eliminar productos del carrito:
+
+        const boton = document.getElementById(`eliminar ${producto.id}`);
+        boton.addEventListener("click", () => {
+            eliminarDelCarrito(producto.id);
+        })
+    })
+    calcularTotal();
 }
 
-let opcion = menu();
-switch(opcion){
-    case 1:
-        carrito();
-        break;
-    case 2:
-        finalizarCompra();
-        break;
-    case 3:
-        salir();
-        break;
-    default:
-    alert("Opción incorrecta");
-    break;
+//Funcion que elimina el producto del carrito:
+
+const eliminarDelCarrito = (id) => {
+    const producto = carrito.find(producto => producto.id === id);
+    const indice = carrito.indexOf(producto);
+    carrito.splice(indice, 1);
+    mostrarCarrito();
+
+    //Trabajamos con el localStorage:
+    localStorage.setItem("carrito", JSON.stringify(carrito));
 }
 
+//Vaciamos todo el carrito de compras.
+
+const vaciarCarrito = document.getElementById("vaciarCarrito");
+
+vaciarCarrito.addEventListener("click", () => {
+    eliminarTodoElCarrito();
+
+})
+
+//Funcion que elimina todo del carrito: si lo hacen con const no funciona
+
+const eliminarTodoElCarrito = () => {
+    carrito = [];
+    mostrarCarrito();
+
+    //localStorage:
+    localStorage.clear();
+}
+
+//Mostrar el total de la comprar
+
+const total = document.getElementById("total");
+
+const calcularTotal = () => {
+    let totalCompra = 0;
+    carrito.forEach(producto => {
+        totalCompra += producto.precio * producto.cantidad;
+        //+= es igual a poner que total compra = totalCompra + producto.precio * producto.cantidad
+    })
+    total.innerHTML = `Total: $${totalCompra}`;
+}
